d2232f1625da4fcd048bc784986e9fe776786f5c
nhchung11 <99390111+nhchung11@users.noreply.github.com>
2023-01-14 17:35:25 +0700

    Add project files.

---
+++ MyProject/LoginWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyProject
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login_btn_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sql = new SqlConnection(@"Data Source=(local);Initial Catalog=CSDL_ChanNuoi;Integrated Security=True");
+            {
+                try
+                {
+                    if(sql.State == ConnectionState.Closed)
+                    {
+                        sql.Open();
+                    }
+                    String querry = "SELECT COUNT (1) FROM Userlogin WHERE username = @UserName AND password = @Password";
+                    SqlCommand sqlCmd = new SqlCommand(querry, sql);
+                    sqlCmd.CommandType = CommandType.Text;
+                    sqlCmd.Parameters.AddWithValue("@UserName", username.Text);
+                    sqlCmd.Parameters.AddWithValue("@Password", password.Text);
+                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    if (count == 1)
+                    {
+                        MainWindow dashboard = new MainWindow();
+                        dashboard.Show();
+                        this.Close();
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect");
+                    }
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+
+        private void cancel_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Signup_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            SigninWindow sw = new SigninWindow();
+            sw.Show();
+        }
+    }
+}
---
+++ MyProject/SigninWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyProject
+{
+    /// <summary>
+    /// Interaction logic for SigninWindow.xaml
+    /// </summary>
+    public partial class SigninWindow : Window
+    {
+        string connectionString = @"Data Source=(local);Initial Catalog=CSDL_ChanNuoi;Integrated Security=True";
+        public SigninWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Backbtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            LoginWindow lw = new LoginWindow();
+            lw.Show();
+        }
+
+        private void SignIn_Nextbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (username.Text == "" || password.Text == "")
+            {
+                MessageBox.Show("Error");
+            }
+            else if (password.Text != confirm_password.Text)
+            {
+                MessageBox.Show("Confirm password do not match");
+            }
+            else
+            {
+                using (SqlConnection sqlcon = new SqlConnection(connectionString))
+                {
+                    sqlcon.Open();
+                    SqlCommand sqlcmd = new SqlCommand("Useradd", sqlcon);
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+                    sqlcmd.Parameters.AddWithValue("@DisplayName", username.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@UserName", username.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@Password", password.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@FullName", FullName.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@PhoneNumber", phonenumber.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@UserAdress", address.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@Birthday", date_of_birth.Text.Trim());
+                    sqlcmd.Parameters.AddWithValue("@IdRole", 2);
+                    sqlcmd.ExecuteNonQuery();
+                    clear();
+                }
+                this.Hide();
+                LoginWindow lw = new LoginWindow();
+                lw.Show();
+            }
+        }
+        void clear()
+        { 
+
+            username.Text = username.Text = password.Text = FullName.Text = phonenumber.Text = address.Text = date_of_birth.Text = "";
+        }
+
+}
+}
